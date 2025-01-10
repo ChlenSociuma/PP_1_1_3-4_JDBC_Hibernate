d6689a0b3af151ed7966ca69ae263396a17b2565
ChlenSociuma <samokolrt@gmail.com>
2025-01-10 23:37:36 +0800

    working 1.0

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -1,7 +1,29 @@
 package jm.task.core.jdbc;
+import jm.task.core.jdbc.model.User;
+import jm.task.core.jdbc.service.UserService;
+import jm.task.core.jdbc.service.UserServiceImpl;
+
+import java.util.List;
+
 
 public class Main {
     public static void main(String[] args) {
-        // реализуйте алгоритм здесь
+        UserService userService = new UserServiceImpl();
+        userService.createUsersTable();
+        User user = new User("Alex", "Popov", (byte) 18);
+        userService.saveUser(user.getName(), user.getLastName(), user.getAge());
+        User user2 = new User("Popo", "Nepopov", (byte) 40);
+        userService.saveUser(user2.getName(), user2.getLastName(), user2.getAge());
+        User user3 = new User("Nick", "Nicov", (byte) 25);
+        userService.saveUser(user3.getName(), user3.getLastName(), user3.getAge());
+        User user4 = new User("Mike", "Vazovskiy", (byte) 20);
+        userService.saveUser(user4.getName(), user4.getLastName(), user4.getAge());
+        List<User> users = userService.getAllUsers();
+        for (User oneUser : users) {
+            System.out.println(oneUser);
+        }
+        userService.cleanUsersTable();
+        userService.dropUsersTable();
     }
+
 }
--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,5 +1,13 @@
 package jm.task.core.jdbc.util;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
 public class Util {
-    // реализуйте настройку соеденения с БД
+    public static Connection getConnection() throws SQLException {
+        String url = "jdbc:postgresql://localhost:5432/habrdb";
+        String user = "habrpguser";
+        String pass = "3232";
+        return DriverManager.getConnection(url, user, pass);
+    }
 }
